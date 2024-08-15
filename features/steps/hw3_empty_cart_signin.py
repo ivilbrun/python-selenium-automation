7e245b25b78c4e8247af8079a6cb1e00d6edf4a3
Ilarie Vilbrun <ilarievilbrun@Ilaries-Air.home>
2024-08-14 21:38:59 -0400

    update steps in hw3_empty_cart_signin.py

--- features/steps/hw3_empty_cart_signin.py
+++ features/steps/hw3_empty_cart_signin.py
@@ -13,38 +13,6 @@ service = Service(driver_path)
 driver = webdriver.Chrome(service=service)
 driver.maximize_window()
 
-# 1. Find the most optimal locators for Create Account on amazon.com (Registration) page elements:
-
-# logo
-driver.find_element(By.XPATH, "//div[@id='a-page']//i[contains(@class, 'a-icon a-icon-logo')]")
-
-# create account
-driver.find_element(By.XPATH, "//form[@id='ap_register_form']//h1[contains(@class, 'a-spacing-small')]")
-
-# name field
-driver.find_element(By.ID, 'ap_customer_name')
-
-# mobile number or email field
-driver.find_element(By.ID, 'ap_email')
-
-# password field
-driver.find_element(By.ID, 'ap_password')
-
-# re-enter password field
-driver.find_element(By.ID, 'ap_password-check')
-
-# continue field
-driver.find_element(By.ID, 'continue')
-
-# conditions of use link
-driver.find_element(By.XPATH, "//div[@id='legalTextRow']//a[contains(@href, 'condition_of_use')]")
-
-# privacy notice link
-driver.find_element(By.XPATH,"//div[@id='legalTextRow']//a[contains(@href, 'privacy notice')]")
-
-# sign in link
-driver.find_element(By.XPATH, "//a[@class='a-link-emphasis']")
-
 # 2. Create a test case using BDD that
 # opens target.com, (given)
 # clicks on the cart icon (when)
@@ -56,9 +24,9 @@ def open_target(context):
     context.driver.get('https://www.target.com/')
 
 
-@when ("Click on Cart icon")
+@when("Click on Cart icon")
 def click_cart(context):
-    context.driver.find_element(By.CSS_SELECTOR, '.icon-cart').click()
+    context.driver.find_element(By.XPATH, "//div[@data-test='@web/CartIcon']").click()
     sleep(2)
 
 
@@ -84,10 +52,44 @@ def sign_in(context):
 @then("Verify Sign In form opened")
 def verify_sign_in(context):
     expected = 'Sign into your Target account'
-    actual = context.driver.find_element(By.XPATH, "//h1[contains(@class, 'StyledHeading')]").text
+    actual = context.driver.find_element(By.XPATH, "//span[text()='Sign into your Target account']").text
     assert expected == actual, f'Expected {expected} did not match actual {actual}'
+
 
 # # OR: driver.find_element(By.XPATH, "//span[text()='Sign into your Target account']")
 # or
 # # Make sure login button is shown
 # driver.find_element(By.ID, 'login')
+
+
+# 1. Find the most optimal locators for Create Account on amazon.com (Registration) page elements:
+#
+# # logo
+# driver.find_element(By.XPATH, "//div[@id='a-page']//i[contains(@class, 'a-icon a-icon-logo')]")
+#
+# # create account
+# driver.find_element(By.XPATH, "//form[@id='ap_register_form']//h1[contains(@class, 'a-spacing-small')]")
+#
+# # name field
+# driver.find_element(By.ID, 'ap_customer_name')
+#
+# # mobile number or email field
+# driver.find_element(By.ID, 'ap_email')
+#
+# # password field
+# driver.find_element(By.ID, 'ap_password')
+#
+# # re-enter password field
+# driver.find_element(By.ID, 'ap_password-check')
+#
+# # continue field
+# driver.find_element(By.ID, 'continue')
+#
+# # conditions of use link
+# driver.find_element(By.XPATH, "//div[@id='legalTextRow']//a[contains(@href, 'condition_of_use')]")
+#
+# # privacy notice link
+# driver.find_element(By.XPATH,"//div[@id='legalTextRow']//a[contains(@href, 'privacy notice')]")
+#
+# # sign in link
+# driver.find_element(By.XPATH, "//a[@class='a-link-emphasis']")
